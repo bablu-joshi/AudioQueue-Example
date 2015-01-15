cf2f56a39be09cfb45606c54f4ee80eb1d7eefba
bablu-joshi <bablujoshi@qainfotech.net>
2015-01-15 13:27:49 +0530

    add files

---
+++ AudioManager.h
@@ -0,0 +1,37 @@
+//
+//  AudioManager.h
+//  AQueue
+//
+//  Created by qainfotech on 1/15/15.
+//  Copyright (c) 2015 aaa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AudioToolbox/AudioQueue.h>
+#import <AudioToolbox/AudioFile.h>
+
+
+#define NUM_BUFFERS 3
+
+typedef struct
+{
+    AudioStreamBasicDescription  dataFormat;
+    AudioQueueRef                queue;
+    AudioQueueBufferRef          buffers[NUM_BUFFERS];
+    AudioFileID                  audioFile;
+    SInt64                       currentPacket;
+    bool                         recording;
+} RecordState;
+
+
+
+@interface AudioManager : NSObject
+{
+    RecordState recordState;
+    CFURLRef fileURL;
+}
+- (void)recordPressed:(id)sender;
+- (void)startRecording;
+- (void)stopRecording;
+-(id)initWithFileName:(NSString *)fileName;
+@end
---
+++ AudioManager.m
@@ -0,0 +1,170 @@
+//
+//  AudioManager.m
+//  AQueue
+//
+//  Created by qainfotech on 1/15/15.
+//  Copyright (c) 2015 aaa. All rights reserved.
+//
+
+#import "AudioManager.h"
+
+
+
+// Declare C callback functions
+void AudioInputCallback(void * inUserData,  // Custom audio metadata
+                        AudioQueueRef inAQ,
+                        AudioQueueBufferRef inBuffer,
+                        const AudioTimeStamp * inStartTime,
+                        UInt32 inNumberPacketDescriptions,
+                        const AudioStreamPacketDescription * inPacketDescs);
+
+void AudioOutputCallback(void * inUserData,
+                         AudioQueueRef outAQ,
+                         AudioQueueBufferRef outBuffer);
+@implementation AudioManager
+
+// Takes a filled buffer and writes it to disk, "emptying" the buffer
+void AudioInputCallback(void * inUserData,
+                        AudioQueueRef inAQ,
+                        AudioQueueBufferRef inBuffer,
+                        const AudioTimeStamp * inStartTime,
+                        UInt32 inNumberPacketDescriptions,
+                        const AudioStreamPacketDescription * inPacketDescs)
+{
+    RecordState * recordState = (RecordState*)inUserData;
+    if (!recordState->recording)
+    {
+        printf("Not recording, returning\n");
+    }
+    
+    // if (inNumberPacketDescriptions == 0 && recordState->dataFormat.mBytesPerPacket != 0)
+    // {
+    //     inNumberPacketDescriptions = inBuffer->mAudioDataByteSize / recordState->dataFormat.mBytesPerPacket;
+    // }
+    
+    printf("Writing buffer %lld\n", recordState->currentPacket);
+    OSStatus status = AudioFileWritePackets(recordState->audioFile,
+                                            false,
+                                            inBuffer->mAudioDataByteSize,
+                                            inPacketDescs,
+                                            recordState->currentPacket,
+                                            &inNumberPacketDescriptions,
+                                            inBuffer->mAudioData);
+    if (status == 0)
+    {
+        recordState->currentPacket += inNumberPacketDescriptions;
+    }
+    
+    AudioQueueEnqueueBuffer(recordState->queue, inBuffer, 0, NULL);
+}
+
+
+- (void)setupAudioFormat:(AudioStreamBasicDescription*)format
+{
+    format->mSampleRate = 8000.0;
+    format->mFormatID = kAudioFormatLinearPCM;
+    format->mFramesPerPacket = 1;
+    format->mChannelsPerFrame = 1;
+    format->mBytesPerFrame = 2;
+    format->mBytesPerPacket = 2;
+    format->mBitsPerChannel = 16;
+    format->mReserved = 0;
+    format->mFormatFlags = kLinearPCMFormatFlagIsBigEndian     |
+    kLinearPCMFormatFlagIsSignedInteger |
+    kLinearPCMFormatFlagIsPacked;
+}
+
+
+
+- (void)startRecording
+{
+    [self setupAudioFormat:&recordState.dataFormat];
+    recordState.currentPacket = 0;
+    OSStatus status;
+    status = AudioQueueNewInput(&recordState.dataFormat,
+                                AudioInputCallback,
+                                &recordState,
+                                CFRunLoopGetCurrent(),
+                                kCFRunLoopCommonModes,
+                                0,
+                                &recordState.queue);
+    
+    if (status == 0)
+    {
+        // Prime recording buffers with empty data
+        for (int i = 0; i < NUM_BUFFERS; i++)
+        {
+            AudioQueueAllocateBuffer(recordState.queue, 16000, &recordState.buffers[i]);
+            AudioQueueEnqueueBuffer (recordState.queue, recordState.buffers[i], 0, NULL);
+        }
+        
+        status = AudioFileCreateWithURL(fileURL,
+                                        kAudioFileAIFFType,
+                                        &recordState.dataFormat,
+                                        kAudioFileFlags_EraseFile,
+                                        &recordState.audioFile);
+        if (status == 0)
+        {
+            recordState.recording = true;
+            status = AudioQueueStart(recordState.queue, NULL);
+            if (status == 0)
+            {
+               // labelStatus.text = @"Recording";
+            }
+        }
+    }
+}
+
+- (void)stopRecording
+{
+    recordState.recording = false;
+    AudioQueueStop(recordState.queue, true);
+    for(int i = 0; i < NUM_BUFFERS; i++)
+    {
+        AudioQueueFreeBuffer(recordState.queue, recordState.buffers[i]);
+    }
+    AudioQueueDispose(recordState.queue, true);
+    AudioFileClose(recordState.audioFile);
+}
+
+
+-(void)resume
+{
+    AudioQueueStart(recordState.queue, NULL);
+}
+
+-(void)pause
+{
+    AudioQueuePause(recordState.queue);
+}
+
+
+
+
+
+-(id)initWithFileName:(NSString *)fileName
+{
+    if (self = [super init]) {
+    // Get audio file page
+        char path[256];
+        [self getFilename:path maxLenth:sizeof path andFileNameTocreate:fileName];
+        fileURL = CFURLCreateFromFileSystemRepresentation(NULL, (UInt8*)path, strlen(path), false);
+        
+        NSLog(@"%@",fileURL);
+    
+    // Init state variables
+    recordState.recording = false;
+    }
+    return self;
+}
+
+- (BOOL)getFilename:(char*)buffer maxLenth:(int)maxBufferLength andFileNameTocreate:(NSString *)fileToCreate
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                                         NSUserDomainMask, YES);
+    NSString* docDir = [paths objectAtIndex:0];
+    
+    NSString* file = [docDir stringByAppendingString:[NSString stringWithFormat:@"/%@.aac",fileToCreate]];
+    return [file getCString:buffer maxLength:maxBufferLength encoding:NSUTF8StringEncoding];
+}
+@end
